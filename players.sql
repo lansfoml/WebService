eb66e13e128a99eeef9b574514de40183203531d
Matt <lansfoml@dukes.jmu.edu>
2021-10-26 18:20:49 -0400

    new table

--- players.sql
+++ players.sql
@@ -3,10 +3,11 @@ DROP TABLE IF EXISTS players;
 CREATE TABLE players (
   id INT,
   number INT,
-  name VARCHAR(255),
+  f_name VARCHAR(255),
+  l_name VARCHAR(255),
   age INT,
   team VARCHAR(255),
-  height VARCHAR(255)
+  
 
 
 );
