efa2d3f72d90603ca352a8d3df0d0c15f537cab9
Matt <lansfoml@dukes.jmu.edu>
2021-10-26 21:44:49 -0400

    lname

--- ws.js
+++ ws.js
@@ -78,6 +78,28 @@ function rowToPlayer(row) {
     });
   });
 
+  service.get('/lname/:lastName', (request, response) => {
+    
+    const parameters = request.params.lastName,
+
+    query = 'SELECT * FROM players WHERE lastName = ?';
+    connection.query(query, parameters, (error, rows) => {
+      if (error) {
+        response.status(500);
+        response.json({
+          ok: false,
+          results: error.message,
+        });
+      } else {
+        const players = rows.map(rowToPlayer);
+        response.json({
+          ok: true,
+          results: rows.map(rowToPlayer),
+        });
+      }
+    });
+  });
+
   service.get('/all', (request, response) => {
   
     const query = 'SELECT * FROM players WHERE is_deleted = 0';
