f5ec94417282ae61446483d5a6022787a6598374
Matt <lansfoml@dukes.jmu.edu>
2021-10-28 10:11:06 -0400

    inseet added

--- players.sql
+++ players.sql
@@ -14,15 +14,27 @@ CREATE TABLE players (
 
 INSERT INTO players(number, firstName, lastName, age, team)
 VALUES(7,"Carmello", "Anthony", 37, "Lakers");
+INSERT INTO players(number, firstName, lastName, age, team)
 VALUES(3,"Anthony", "Davis", 28, "Lakers");
+INSERT INTO players(number, firstName, lastName, age, team)
 VALUES(6,"Lebron", "James", 36, "Lakers");
+INSERT INTO players(number, firstName, lastName, age, team)
 VALUES(1,"Trevor", "Ariza", 36, "Lakers");
+INSERT INTO players(number, firstName, lastName, age, team)
 VALUES(7,"Kevin", "Durant", 33, "Nets");
+INSERT INTO players(number, firstName, lastName, age, team)
 VALUES(11,"Kyrie", "Irving", 29, "Nets");
+INSERT INTO players(number, firstName, lastName, age, team)
 VALUES(13,"James", "Harden", 32, "Nets");
+INSERT INTO players(number, firstName, lastName, age, team)
 VALUES(12,"Joe", "Harris", 30, "Nets");
+INSERT INTO players(number, firstName, lastName, age, team)
 VALUES(2,"LaMelo", "Ball", 20, "Hornets");
+INSERT INTO players(number, firstName, lastName, age, team)
 VALUES(25,"PJ", "Washington", 23, "Hornets");
+INSERT INTO players(number, firstName, lastName, age, team)
 VALUES(1,"John", "Wall", 31, "Rokcets");
+INSERT INTO players(number, firstName, lastName, age, team)
 VALUES(0,"Jalen", "Green", 19, "Rokcets");
+INSERT INTO players(number, firstName, lastName, age, team)
 VALUES(10,"Eric", "Gordon", 32, "Rokcets");
