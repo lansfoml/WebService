32b0bcaf1d003424c87452b59da1403ec34ef092
Matt <lansfoml@dukes.jmu.edu>
2021-10-28 10:15:33 -0400

    no password

--- create_unforget.sql
+++ create_unforget.sql
@@ -4,5 +4,5 @@ DROP USER IF EXISTS unforget_user@localhost;
 
 -- Create Unforget database and user. Ensure Unicode is fully supported.
 CREATE DATABASE unforget CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
-CREATE USER unforget_user@localhost IDENTIFIED WITH mysql_native_password BY 'password';
+CREATE USER unforget_user@localhost IDENTIFIED WITH mysql_native_password BY '';
 GRANT ALL PRIVILEGES ON unforget.* TO unforget_user@localhost;
