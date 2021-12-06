d25f345a5b7c9c2a46b434d6ceb81e4b7e71f561
Matt <lansfoml@dukes.jmu.edu>
2021-12-06 14:20:53 -0500

    new pos

--- players.sql
+++ players.sql
@@ -15,7 +15,7 @@ CREATE TABLE players (
 
 
 
--- LAKERS
+
 INSERT INTO players(number, firstName, lastName, age, team, position)
 VALUES(7,"Carmello", "Anthony", 37, "Lakers", "PF");
 INSERT INTO players(number, firstName, lastName, age, team, position)
@@ -33,3 +33,27 @@ VALUES(10,"Deandre", "Jordan", 33, "Lakers", "C");
 INSERT INTO players(number, firstName, lastName, age, team, position)
 VALUES(0,"Russell", "Westbrook", 33, "Lakers", "PG");
 
+
+
+INSERT INTO players(number, firstName, lastName, age, team, position)
+VALUES(7,"Kevin", "Durant", 33, "Nets", "PF");
+INSERT INTO players(number, firstName, lastName, age, team, position)
+VALUES(11,"Kyrie", "Irving", 29, "Nets", "PG");
+INSERT INTO players(number, firstName, lastName, age, team, position)
+VALUES(13,"James", "Harden", 32, "Nets", "SG");
+INSERT INTO players(number, firstName, lastName, age, team, position)
+VALUES(12,"Joe", "Harris", 30, "Nets", "SF");
+
+
+INSERT INTO players(number, firstName, lastName, age, team, position)
+VALUES(2,"LaMelo", "Ball", 20, "Hornets", "PG");
+INSERT INTO players(number, firstName, lastName, age, team, position)
+VALUES(25,"PJ", "Washington", 23, "Hornets", "PG");
+
+
+INSERT INTO players(number, firstName, lastName, age, team, position)
+VALUES(1,"John", "Wall", 31, "Rockets", "PG");
+INSERT INTO players(number, firstName, lastName, age, team, position)
+VALUES(0,"Jalen", "Green", 19, "Rockets", "SG");
+INSERT INTO players(number, firstName, lastName, age, team), position
+VALUES(10,"Eric", "Gordon", 32, "Rockets", "PF");
